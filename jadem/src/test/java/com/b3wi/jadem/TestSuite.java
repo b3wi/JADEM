2e51c15f04a92b485f2fc504ec8391f9bf49ac0a
damienvdb06 <damienvdb06@users.noreply.github.com>
2015-04-10 23:04:19 +0200

    Updating junit from 3.8 to 4.12 and target JRE 1.5 to 1.8.

---
+++ jadem/src/test/java/com/b3wi/jadem/TestSuite.java
@@ -0,0 +1,12 @@
+package com.b3wi.jadem;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value = Suite.class)
+@SuiteClasses(value = { AppTest.class })
+public class TestSuite
+{
+
+}
